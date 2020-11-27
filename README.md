87473bc76b79589caa48a443f0c2c7b1188f4037
Alexandre-Borghi <borghi.alexandre.12@gmail.com>
2020-11-27 19:11:32 +0100

    Now gets image feed from camera

--- README.md
+++ README.md
@@ -9,3 +9,27 @@ It serves a web server and a web app used to change the settings and view inform
 It uses Rust because I want to learn Rust and I want the software to run fast.
 
 It is published under the MPL 2.0 license.
+
+It only works on Linux because it uses the [v4l](https://crates.io/crates/v4l) crate which uses [v4l2](https://en.wikipedia.org/wiki/Video4Linux).
+
+---
+
+## Building
+
+To build this program, please install the following dependencies
+
+### v4l2
+
+You will need the v4l2 development library to build this prject. If your OS uses the APT package manager, you can install v4l2 like that :
+
+```shell
+	$ sudo apt install libv4l-dev
+```
+
+### libclang
+
+You will need to install a libclang package. I personally did this with APT :
+
+```shell
+	$ sudo apt-get install libclang-*
+```
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,47 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+extern crate v4l;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+use v4l::device::List;
+use v4l::prelude::*;
+
 fn main() {
-	println!("Hello, world!");
+	let list = List::new();
+
+	if list.count() == 0 {
+		println!("No available device !");
+		return;
+	}
+
+	let list = List::new();
+
+	println!("Available devices :");
+	for dev in list {
+		println!("    {}: {}", dev.index().unwrap(), dev.name().unwrap());
+	}
+	println!("");
+
+	let mut dev = CaptureDevice::new(0).unwrap();
+	let fmt = dev.format().unwrap();
+
+	println!("Using format :\n{}", fmt);
+
+	let mut stream = MmapStream::with_buffers(&mut dev, 4).expect("Failed to create buffer stream");
+
+	loop {
+		let frame = stream.next().unwrap();
+		println!(
+			"Buffer size: {}, seq: {}, timestamp: {}",
+			frame.len(),
+			frame.meta().sequence,
+			frame.meta().timestamp
+		);
+
+		let mut file = File::create(format!("frame.jpg")).unwrap();
+		file.write_all(frame.data()).unwrap();
+	}
 }
